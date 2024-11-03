f94439acf923ceadf63a49c978608574c91f9717
vision13141 <s.vision.dev@gmail.com>
2024-11-03 22:53:55 +0600

    City Properties

--- src/Component/SlidePart.jsx
+++ src/Component/SlidePart.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Container from './Container'
-import item from '../assets/Image/item.png'
+import item1 from '../assets/Image/Item1.png'
 import item2 from '../assets/Image/item2.png'
 import item3 from '../assets/Image/item3.png'
 import item4 from '../assets/Image/item4.png'
@@ -9,6 +9,7 @@ import item6 from '../assets/Image/item6.png'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
 const SlidePart = () => {
     const settings = {
         dots: false,
@@ -26,7 +27,7 @@ const SlidePart = () => {
                 <h1 className='text-[#292929] text-[25px] font-bold text-center pb-8'>
                     Don’t worry to Fail. We have Experienced with Big Company’s</h1>
                 <Slider {...settings}>
-                    <img src={item} />
+                    <img src={item1} />
                     <img src={item2} />
                     <img src={item3} />
                     <img src={item4} />
